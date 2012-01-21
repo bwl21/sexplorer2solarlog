95524749073f25192492d02457a3e8e041f669d3
Bernhard Weichel <Bernhard.Weichel@git.weichel21.de>
2012-01-21 13:27:59 +0100

    installationsscript fertig

--- Deploy/mkinstall.php
+++ Deploy/mkinstall.php
@@ -1,20 +1,18 @@
 <?php
 
-// $Id: mkinstall.php dfffcb4bfaa1 2012/01/21 12:03:42 Bernhard $
+// $Id: mkinstall.php 5376e40b8d28 2012/01/21 12:27:59 Bernhard $
 
 // $Author: Bernhard $
-// $Date: 2012/01/21 12:03:42 $
-// $Header: /Users/beweiche/02_svn-sandbox/SExplore2Slog/Deploy/mkinstall.php,v dfffcb4bfaa1 2012/01/21 12:03:42 Bernhard $
-// $Id: mkinstall.php dfffcb4bfaa1 2012/01/21 12:03:42 Bernhard $
+// $Date: 2012/01/21 12:27:59 $
+// $Header: /Users/beweiche/02_svn-sandbox/SExplore2Slog/Deploy/mkinstall.php,v 5376e40b8d28 2012/01/21 12:27:59 Bernhard $
+// $Id: mkinstall.php 5376e40b8d28 2012/01/21 12:27:59 Bernhard $
 // $LTag: Version-01 $
-// $LocalRevision: 54 $
+// $LocalRevision: 55 $
 // $RCSfile: mkinstall.php $
-// $Revision: dfffcb4bfaa1 $
+// $Revision: 5376e40b8d28 $
 // $Source: /Users/beweiche/02_svn-sandbox/SExplore2Slog/Deploy/mkinstall.php $
 
 
-
-var_dump($argv);
 $version = $argv[1];
 system("hg update ".$version);
 system('zip -r -p Deploy/SExplore2Slog-'.$version.'.zip *.php Classes readme.txt');
