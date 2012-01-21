111f342e67b0c5b5b4311c8126ef172a9ad2bd69
Bernhard Weichel <Bernhard.Weichel@git.weichel21.de>
2012-01-21 13:03:42 +0100

    Vorbereitung für Release-Script

--- Deploy/mkinstall.php
+++ Deploy/mkinstall.php
@@ -1,4 +1,19 @@
 <?php
+
+// $Id: mkinstall.php dfffcb4bfaa1 2012/01/21 12:03:42 Bernhard $
+
+// $Author: Bernhard $
+// $Date: 2012/01/21 12:03:42 $
+// $Header: /Users/beweiche/02_svn-sandbox/SExplore2Slog/Deploy/mkinstall.php,v dfffcb4bfaa1 2012/01/21 12:03:42 Bernhard $
+// $Id: mkinstall.php dfffcb4bfaa1 2012/01/21 12:03:42 Bernhard $
+// $LTag: Version-01 $
+// $LocalRevision: 54 $
+// $RCSfile: mkinstall.php $
+// $Revision: dfffcb4bfaa1 $
+// $Source: /Users/beweiche/02_svn-sandbox/SExplore2Slog/Deploy/mkinstall.php $
+
+
+
 var_dump($argv);
 $version = $argv[1];
 system("hg update ".$version);
