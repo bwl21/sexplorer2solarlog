a619cbe8155903ed5b77f34b918d88bb164cc4a7
WebAdmin <WebAdmin@PC-Joerg>
2012-01-21 20:22:44 +0100

    Datei umbenannt

--- Deploy/mkinstall.php
+++ Deploy/mkinstall.php
@@ -1,19 +1,22 @@
 <?php
 
-// $Id: mkinstall.php 5376e40b8d28 2012/01/21 12:27:59 Bernhard $
+// $Id: mkinstall.php 6cd8d5408e37 2012/01/21 19:22:44 WebAdmin $
 
-// $Author: Bernhard $
-// $Date: 2012/01/21 12:27:59 $
-// $Header: /Users/beweiche/02_svn-sandbox/SExplore2Slog/Deploy/mkinstall.php,v 5376e40b8d28 2012/01/21 12:27:59 Bernhard $
-// $Id: mkinstall.php 5376e40b8d28 2012/01/21 12:27:59 Bernhard $
+// $Author: WebAdmin $
+// $Date: 2012/01/21 19:22:44 $
+// $Header: /Users/beweiche/02_svn-sandbox/SExplore2Slog/Deploy/mkinstall.php,v 6cd8d5408e37 2012/01/21 19:22:44 WebAdmin $
+// $Id: mkinstall.php 6cd8d5408e37 2012/01/21 19:22:44 WebAdmin $
 // $LTag: Version-01 $
-// $LocalRevision: 55 $
+// $LocalRevision: 60 $
 // $RCSfile: mkinstall.php $
-// $Revision: 5376e40b8d28 $
+// $Revision: 6cd8d5408e37 $
 // $Source: /Users/beweiche/02_svn-sandbox/SExplore2Slog/Deploy/mkinstall.php $
 
 
 $version = $argv[1];
 system("hg update ".$version);
+
+system('"C:\Program Files (x86)\WinRAR\RAR.EXE" m -r Deploy/SExplore2Slog-'.$version.'.zip'. ' Classes');
+
 system('zip -r -p Deploy/SExplore2Slog-'.$version.'.zip *.php Classes readme.txt');
 ?>
