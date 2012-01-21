49670d6f996213b3cc2a2eeb51ca054c324cb073
Bernhard Weichel <Bernhard.Weichel@git.weichel21.de>
2012-01-21 12:55:14 +0100

    Vorbereitung für Release-Script

--- Deploy/mkinstall.php
+++ Deploy/mkinstall.php
@@ -1,3 +1,6 @@
 <?php
-system('zip -r -p Deploy/SExplore2Slog.zip *.php Classes')
+var_dump($argv);
+$version = $argv[1];
+system("hg update ".$version);
+system('zip -r -p Deploy/SExplore2Slog-'.$version.'.zip *.php Classes readme.txt');
 ?>
