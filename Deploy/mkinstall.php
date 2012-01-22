90eda25c2a5f31e26f8747d476d5d1108469d9af
Bernhard Weichel <Bernhard.Weichel@git.weichel21.de>
2012-01-22 19:47:01 +0100

    fixed #4 fixed filename for config.inc.php_example

--- Deploy/mkinstall.php
+++ Deploy/mkinstall.php
@@ -1,15 +1,15 @@
 <?php
 
-// $Id: mkinstall.php 405fe2807fa2 2012/01/22 18:22:55 Bernhard $
+// $Id: mkinstall.php 908d08dfe8e4 2012/01/22 18:47:01 Bernhard $
 
 // $Author: Bernhard $
-// $Date: 2012/01/22 18:22:55 $
-// $Header: /Users/beweiche/02_svn-sandbox/SExplore2Slog/Deploy/mkinstall.php,v 405fe2807fa2 2012/01/22 18:22:55 Bernhard $
-// $Id: mkinstall.php 405fe2807fa2 2012/01/22 18:22:55 Bernhard $
+// $Date: 2012/01/22 18:47:01 $
+// $Header: /Users/beweiche/02_svn-sandbox/SExplore2Slog/Deploy/mkinstall.php,v 908d08dfe8e4 2012/01/22 18:47:01 Bernhard $
+// $Id: mkinstall.php 908d08dfe8e4 2012/01/22 18:47:01 Bernhard $
 // $LTag: Version-01 $
-// $LocalRevision: 65 $
+// $LocalRevision: 70 $
 // $RCSfile: mkinstall.php $
-// $Revision: 405fe2807fa2 $
+// $Revision: 908d08dfe8e4 $
 // $Source: /Users/beweiche/02_svn-sandbox/SExplore2Slog/Deploy/mkinstall.php $
 
 
@@ -17,5 +17,5 @@ $version = $argv[1];
 system("hg update ".$version);
 
 
-system('zip -r -p Deploy/SExplore2Slog-'.$version.'.zip index.php conf.inc.php_example Classes readme.txt');
+system('zip -r -p Deploy/SExplore2Slog-'.$version.'.zip index.php config.inc.php_example Classes readme.txt');
 ?>
