ba58e5ca412c4d8d5ad340e93175f36e5ac8af7d
Bernhard Weichel <Bernhard.Weichel@git.weichel21.de>
2012-01-30 22:21:29 +0100

    mkinstall baut nun auch base_vars.js_example ein

--- Deploy/mkinstall.php
+++ Deploy/mkinstall.php
@@ -1,15 +1,15 @@
 <?php
 
-// $Id: mkinstall.php 908d08dfe8e4 2012/01/22 18:47:01 Bernhard $
+// $Id: mkinstall.php 7fea35d5bf2f 2012/01/30 21:21:29 Bernhard $
 
 // $Author: Bernhard $
-// $Date: 2012/01/22 18:47:01 $
-// $Header: /Users/beweiche/02_svn-sandbox/SExplore2Slog/Deploy/mkinstall.php,v 908d08dfe8e4 2012/01/22 18:47:01 Bernhard $
-// $Id: mkinstall.php 908d08dfe8e4 2012/01/22 18:47:01 Bernhard $
-// $LTag: Version-01 $
-// $LocalRevision: 70 $
+// $Date: 2012/01/30 21:21:29 $
+// $Header: /Users/beweiche/02_svn-sandbox/SExplore2Slog/Deploy/mkinstall.php,v 7fea35d5bf2f 2012/01/30 21:21:29 Bernhard $
+// $Id: mkinstall.php 7fea35d5bf2f 2012/01/30 21:21:29 Bernhard $
+// $LTag: Version-02 $
+// $LocalRevision: 95 $
 // $RCSfile: mkinstall.php $
-// $Revision: 908d08dfe8e4 $
+// $Revision: 7fea35d5bf2f $
 // $Source: /Users/beweiche/02_svn-sandbox/SExplore2Slog/Deploy/mkinstall.php $
 
 
@@ -17,5 +17,5 @@ $version = $argv[1];
 system("hg update ".$version);
 
 
-system('zip -r -p Deploy/SExplore2Slog-'.$version.'.zip index.php config.inc.php_example Classes readme.txt');
+system('zip -r -p Deploy/SExplore2Slog-'.$version.'.zip index.php config.inc.php_example Classes readme.txt base_vars.js_example');
 ?>
