6f875372438020336a8a37964ee069d8c7a77d70
Bernhard Weichel <Bernhard.Weichel@git.weichel21.de>
2012-02-25 16:17:31 +0100

    fixed #11 - improve Release-Prozess

--- Deploy/mkinstall.php
+++ Deploy/mkinstall.php
@@ -1,15 +1,14 @@
 <?php
 
-// $Id: mkinstall.php 85e2422f3df6 2012/01/30 23:07:10 Bernhard $
+// $Id: mkinstall.php eee27f94be84 2012/02/25 15:17:31 Bernhard $
 
-// $Author: Bernhard $
-// $Date: 2012/01/30 23:07:10 $
-// $Header: /Users/beweiche/02_svn-sandbox/SExplore2Slog/Deploy/mkinstall.php,v 85e2422f3df6 2012/01/30 23:07:10 Bernhard $
-// $Id: mkinstall.php 85e2422f3df6 2012/01/30 23:07:10 Bernhard $
+
+// $Date: 2012/02/25 15:17:31 $
+// $Id: mkinstall.php eee27f94be84 2012/02/25 15:17:31 Bernhard $
 // $LTag: Version-03 $
-// $LocalRevision: 96 $
+// $LocalRevision: 104 $
 // $RCSfile: mkinstall.php $
-// $Revision: 85e2422f3df6 $
+// $Revision: eee27f94be84 $
 // $Source: /Users/beweiche/02_svn-sandbox/SExplore2Slog/Deploy/mkinstall.php $
 
 
@@ -17,5 +16,6 @@ $version = $argv[1];
 system("hg update ".$version);
 
 
-system('zip -r -p Deploy/SExplore2Slog-'.$version.'.zip index.php config.inc.php_example readme.txt base_vars.js_example Classes localFiles');
+
+system('zip -r -p Releases/SExplore2Slog-'.$version.'.zip index.php config.inc.php_example readme.txt base_vars.js_example Classes localFiles');
 ?>
